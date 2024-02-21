6f17e2375b3b842f2aff8bab5d4c447efc6a4240
Benalagat <benardkiplagat2000@gmail.com>
2024-02-22 00:06:45 +0300

    liveview

---
+++ lib/elixir_gist/gists/saved_gist.ex
@@ -0,0 +1,22 @@
+defmodule ElixirGist.Gists.SavedGist do
+  # alias ElixirGist.Accounts
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "saved_gists" do
+
+    belongs_to :user, ElixirGist.Accounts.User
+    belongs_to :gists, ElixirGist.Gists.Gist
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(saved_gist, attrs) do
+    saved_gist
+    |> cast(attrs, [:user_id, :gist_id])
+    |> validate_required([:user_id, :gist_id])
+  end
+end
